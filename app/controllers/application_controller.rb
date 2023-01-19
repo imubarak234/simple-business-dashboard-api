06c35b32da666d1751ce2b43f1a36a364759096d
imubarak234 <imubarak2424@gmail.com>
2023-01-19 18:02:14 +0100

    db seeds setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
 class ApplicationController < ActionController::API
+
+  include ActionController::HttpAuthentication::Token
+  before_action :set_default_format
+
+  def json_payload
+    HashWithIndifferentAccess.new(JSON.parse(request.raw_post))
+  end
+
+  def set_default_format
+    request.format = :json
+  end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,32 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+def access()
+  var = [ 'Admin', 'Manager', 'cashier', 'User' ]
+  var[rand(0.3)]
+end
+
+require "faker"
+
+25.times do
+  users = User.new(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    age: Faker::Number.between(from: 18, to: 70),
+    email: Faker::Internet.email,
+    address: Faker::Address.full_address,
+    address_2: Faker::Address.full_address,
+    zipcode: Faker::Address.zip_code,
+    city: Faker::Address.city,
+    phone: Faker::PhoneNumber.cell_phone_with_country_code,
+  )
+  users.save
+end
+
+15.times do
+  staff = Staff.new(
+    access_level: access()
+    
+  )
+end
